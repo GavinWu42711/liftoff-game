77d8bf300c8f448b720e1cc88e82c60698c1a3ea
GavinWu42711 <gavinwu42711@gmail.com>
2026-05-10 03:36:12 -0400

    Main menu
    Made it so you can now transition into the motor training scene

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -7,6 +7,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	communicationButton.pressed.connect(changeToComScene)
+	motorButton.pressed.connect(changeToMotorScene)
 
 
 # Cal	led every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +17,5 @@ func _process(delta: float) -> void:
 func changeToComScene() -> void:
 	get_tree().change_scene_to_file("res://Scenes/CommunicationTraining.tscn")
 	
+func changeToMotorScene() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MotorTraining.tscn")
