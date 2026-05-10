b72566c05c541f78b10ac9bad0fba80cb6a2c890
GavinWu42711 <gavinwu42711@gmail.com>
2026-05-10 03:09:05 -0400

    Main Menu
    Added a main menu

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var motorButton:Button = $Motor
+@onready var communicationButton:Button = $Communication
+@onready var socialButton:Button = $Social
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	communicationButton.pressed.connect(changeToComScene)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func changeToComScene() -> void:
+	get_tree().change_scene_to_file("res://Scenes/CommunicationTraining.tscn")
+	
